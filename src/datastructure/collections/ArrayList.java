eb5552aa496fdc7236860094b4664c4dfcfb6429
Subrat <logosubrat@gmail.com>
2022-06-10 17:33:53 -0400

    for each loop, for loop and while loop

---
+++ src/datastructure/collections/ArrayList.java
@@ -0,0 +1,29 @@
+package datastructure.collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.*;
+
+public class ArrayList {
+
+
+
+    public static void main(String[] args) {
+        java.util.ArrayList<String> languages = new java.util.ArrayList<>();
+        languages.add("German");
+        languages.add("Nepali");
+        languages.add("English");
+        languages.add("Korean");
+        languages.add("Spanish");
+        languages.add("Hindi");
+        System.out.println("Original ArrayList: " + languages);
+        languages.remove("Hindi");
+        System.out.println("Modified Arraylist: " + languages);
+
+
+
+    }
+
+
+
+
+}
